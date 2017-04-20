53cfd5145bf289ceb5f505a5ba355b58a4d1f082
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-04-20 20:10:27 +0100

    Nice little calls.go refactor.

--- calls.go
+++ calls.go
@@ -14,75 +14,35 @@ func (c *CallErrorResponse) Error() string {
 // CreateCall starts a voice call, configured using the provided CreateCallRequest.
 func (c *CallService) CreateCall(request CreateCallRequest) (*CreateCallResponse, *http.Response, error) {
 	sling := c.sling.New().Post("").BodyJSON(request)
-	if err := c.authSet.ApplyJWT(sling); err != nil {
-		return nil, nil, fmt.Errorf("%s - error applying JWT", err)
-	}
 
 	callResponse := new(CreateCallResponse)
-	errorResponse := new(CallErrorResponse)
-	httpResponse, err := sling.Receive(callResponse, errorResponse)
-	if err != nil {
-		return nil, httpResponse, fmt.Errorf("%s - error receiving data from server", err)
-	}
-	if errorResponse.Type != "" {
-		return nil, httpResponse, errorResponse
-	}
-	return callResponse, httpResponse, nil
+	httpResponse, err := c.makeRequest(sling, callResponse)
+	return callResponse, httpResponse, err
 }
 
 // SearchCalls returns information about calls matching the filter in the provided SearchCallsRequest
 func (c *CallService) SearchCalls(request SearchCallsRequest) (*SearchCallsResponse, *http.Response, error) {
 	sling := c.sling.New().Get("").QueryStruct(request)
-	if err := c.authSet.ApplyJWT(sling); err != nil {
-		return nil, nil, fmt.Errorf("%s - error applying JWT", err)
-	}
 
 	callResponse := new(SearchCallsResponse)
-	errorResponse := new(CallErrorResponse)
-	httpResponse, err := sling.Receive(callResponse, errorResponse)
-	if err != nil {
-		return nil, httpResponse, fmt.Errorf("%s - error receiving data from server", err)
-	}
-	if errorResponse.Type != "" {
-		return nil, httpResponse, errorResponse
-	}
-	return callResponse, httpResponse, nil
+	httpResponse, err := c.makeRequest(sling, callResponse)
+	return callResponse, httpResponse, err
 }
 
 func (c *CallService) GetCallInfo(uuid string) (*CallInfo, *http.Response, error) {
 	sling := c.sling.New().Get(uuid)
-	if err := c.authSet.ApplyJWT(sling); err != nil {
-		return nil, nil, fmt.Errorf("%s - error applying JWT", err)
-	}
 
 	callResponse := new(CallInfo)
-	errorResponse := new(CallErrorResponse)
-	httpResponse, err := sling.Receive(callResponse, errorResponse)
-	if err != nil {
-		return nil, httpResponse, fmt.Errorf("%s - error receiving data from server", err)
-	}
-	if errorResponse.Type != "" {
-		return nil, httpResponse, errorResponse
-	}
-	return callResponse, httpResponse, nil
+	httpResponse, err := c.makeRequest(sling, callResponse)
+	return callResponse, httpResponse, err
 }
 
 func (c *CallService) ModifyCall(uuid string, request interface{}) (*ModifyCallResponse, *http.Response, error) {
 	sling := c.sling.New().Put(uuid).BodyJSON(request)
-	if err := c.authSet.ApplyJWT(sling); err != nil {
-		return nil, nil, fmt.Errorf("%s - error applying JWT", err)
-	}
 
 	callResponse := new(ModifyCallResponse)
-	errorResponse := new(CallErrorResponse)
-	httpResponse, err := sling.Receive(callResponse, errorResponse)
-	if err != nil {
-		return nil, httpResponse, fmt.Errorf("%s - error receiving data from server", err)
-	}
-	if errorResponse.Type != "" {
-		return nil, httpResponse, errorResponse
-	}
-	return callResponse, httpResponse, nil
+	httpResponse, err := c.makeRequest(sling, callResponse)
+	return callResponse, httpResponse, err
 }
 
 func (c *CallService) makeRequest(s *sling.Sling, successV interface{}) (*http.Response, error) {
