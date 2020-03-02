487c859db9a6f00d17f27439f44764877c38925b
Lorna Jane Mitchell <lorna@lornajane.net>
2020-03-02 14:38:42 +0000

    Start with a new SMS approach and tests

---
+++ auth_test.go
@@ -0,0 +1,30 @@
+package nexmo
+
+import "testing"
+
+func TestGetCreds(t *testing.T) {
+	// create the auth struct
+	myAuth := CreateAuthFromKeySecret("123", "456")
+	myCreds := myAuth.getCreds()
+
+	if myCreds[0] != "123" {
+		t.Error("Key creds are incorrect")
+	}
+
+	if myCreds[1] != "456" {
+		t.Error("Secret creds are incorrect")
+	}
+}
+
+func TestCreateAuthFromKeySecret(t *testing.T) {
+	// create the auth struct
+	myAuth := CreateAuthFromKeySecret("123", "456")
+
+	if myAuth.apiKey != "123" {
+		t.Error("Auth key not set")
+	}
+
+	if myAuth.apiSecret != "456" {
+		t.Error("Auth secret not set")
+	}
+}
