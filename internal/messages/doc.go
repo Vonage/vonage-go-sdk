17bc2cbca8befa9ecdfa7dbca089df20c982a86c
Bohdan Darahan <Bohdan.Darahan@vonage.com>
2021-09-22 09:58:15 +0100

    Generated DTOs for Messages

---
+++ internal/messages/doc.go
@@ -0,0 +1,3 @@
+package messages
+
+//go:generate oapi-codegen -generate types -package messages -o messages.go https://raw.githubusercontent.com/Nexmo/api-specification/main/definitions/messages-olympus.yml
